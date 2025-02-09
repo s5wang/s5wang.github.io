1e00cb3db2083cc013569ff116137529af4fabc0
beanandbean <wangsw.a@gmail.com>
2025-02-09 09:46:56 +0000

    HTML style improvements

--- public/deobfus.js
+++ public/deobfus.js
@@ -1,7 +1,9 @@
+import { sanitizeUrl } from "https://cdn.jsdelivr.net/npm/@braintree/sanitize-url@7.1.1/+esm";
+
 // de-obfuscation
 for (const element of document.getElementsByClassName("obfuscate")) {
   element.innerText = atob(element.innerText);
   if (element.tagName.toLowerCase() === "a") {
-    element.href = atob(element.attributes.href.value);
+    element.href = sanitizeUrl(atob(element.attributes.href.value));
   }
 }
